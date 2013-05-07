5081eee821d766dc5f8439ee975ce959c6dcecfd
Jason <jasonm@drivecomputing.co.uk>
2013-05-07 13:05:30 +0100

    copied Job hussaintest1 app code in
    plan to merge with hussains code to create the ultimate Bliss app

---
+++ js/pageLoadInit.js
@@ -0,0 +1,16 @@
+//This should be loaded before jquery mobile js in your page
+//jquery code in here only please
+
+$(document).bind("mobileinit", function(){
+  //apply overrides here
+  $.mobile.allowCrossDomainPages = true;
+  $.mobile.defaultPageTransition = 'pop';
+  $.mobile.touchOverflowEnabled = true;
+});
+
+$('#custenq').live('pageshow',function(event, ui){
+});
+
+
+
+
